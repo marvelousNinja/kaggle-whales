71bd9a40200ffb7ab2af7faad58a1906b320051d
Alex Sologub <alexsologoub@gmail.com>
2022-03-25 11:44:11 +0300

    Add dataset to download script

--- download.sh
+++ download.sh
@@ -8,3 +8,6 @@ cd ./data
 
 kaggle competitions download -c happy-whale-and-dolphin
 unzip happy-whale-and-dolphin.zip
+
+kaggle datasets download -d phalanx/whale2-cropped-dataset
+unzip whale2-cropped-dataset.zip
